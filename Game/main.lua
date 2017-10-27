7540405a073927e31c6ebbd0b5410e45ac852349
Damian <damianvu@ku.edu>
2017-10-27 18:07:41 -0500

    Initial functionality has been created. FPS counter, Player movement with WASD, and custom mouse cursor on screen

--- Game/main.lua
+++ Game/main.lua
@@ -1,7 +1,80 @@
+-- Current functionality is just movement and mouse cursor until we get maps and tiling implemented
+
+mouse = {}
+player = {}
+
+function love.load()
+
+    -- Make mouse invisible so we can use a custom cursor --
+    love.mouse.setVisible(false)
+
+    base_speed = 250
+
+
+    -- Player initialization
+    player.x = 400
+    player.y = 300
+    player.speed = 1 -- (We can scale this number later to have speed modifiers)
+    player.img = love.graphics.newImage('player.png')
+    -- End Player initialization
+
+
+    -- Code that will cap FPS at 144
+    min_dt = 1/144
+    next_time = love.timer.getTime()
+    -- End Code that will cap FPS at 144
+end
+
 function love.draw()
-    love.graphics.print("Let's do this", 400, 300)
+    -- Draw circle where mouse is --
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.circle("line", mouse.x, mouse.y, 5) -- "line" is outline, 5 is radius
+
+    -- Draw player
+    love.graphics.draw(player.img, player.x, player.y, 0, 1, 1, 0, 32)
+
+
+
+    -- Text in the top left
+    love.graphics.setColor(0, 203, 255)
+    love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 10)
+    love.graphics.print("Wubba lubba dub dub!", 10, 30)
+    -- End Text in the top left
+
+    -- Code that will cap FPS at 144 --
+    local cur_time = love.timer.getTime()
+    if next_time <= cur_time then
+        next_time = cur_time
+        return
+    end
+    love.timer.sleep(next_time - cur_time)
+    -- End Code that will cap FPS at 144 --
+end
+
+function love.update(dt)
+    -- Code that will cap FPS at 144
+    next_time = next_time + min_dt
+
+    -- Get current mouse position and store in object mouse
+    mouse.x, mouse.y = love.mouse.getPosition()
+
+    -- Listen for keypresses to move player (if a player holds 'w' and 's', they will be stationary. Same with 'a' and 'd')
+    if love.keyboard.isDown('d') then
+        player.x = player.x + (player.speed * base_speed * dt)
+    end
+    if love.keyboard.isDown('a') then
+        player.x = player.x - (player.speed * base_speed * dt)
+    end
+    if love.keyboard.isDown('w') then
+        player.y = player.y - (player.speed * base_speed * dt)
+    end
+    if love.keyboard.isDown('s') then
+        player.y = player.y + (player.speed * base_speed * dt)
+    end
+
+
 end
 
 function love.quit()
-  print("Thanks for playing! Come back soon!")
+  print("Game instance has been closed")
 end
