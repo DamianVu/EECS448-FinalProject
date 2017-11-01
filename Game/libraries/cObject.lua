d1d8e0606d89bb3e26e59a3e61b224e37de44ee4
Damian <damianvu@ku.edu>
2017-11-01 01:31:40 -0500

    Formatting changes. About to start reformatting the highlight code

--- Game/libraries/cObject.lua
+++ Game/libraries/cObject.lua
@@ -1,9 +1,11 @@
 -- Named: cObject for collisionObject
+-- Features:
+-- Auto scaling of cObject based on chosen sprite dimensions and chosen width/height
+defaultSize = 64
+
 class = require 'libraries.ext.30log'
 
-cObject = class("cObject", {})
-
-defaultSize = 64
+cObject = class("cObject", {x_vel = 0, y_vel = 0, x_vel_counter = 10, y_vel_counter = 10})
 
 -- Initialization
 function cObject:init(id, sprite, speed, x, y, width, height, x_offset, y_offset, rotation)
@@ -20,12 +22,7 @@ function cObject:init(id, sprite, speed, x, y, width, height, x_offset, y_offset
 	self.scaleX = self.width / imgW
 	self.scaleY = self.height / imgH
 	self.rotation = rotation or 0
-	self.x_vel = 0
-	self.y_vel = 0
-	self.x_vel_counter = 10
-	self.y_vel_counter = 10
 end
-
 
 -- Will return 4 coordinates that the object currently spans. In top-left, top-right, bottom-left, bottom-right order
 function cObject:getSpan()
@@ -42,17 +39,10 @@ function cObject:drawHitbox()
 	love.graphics.setColor(247, 176, 34)
 	love.graphics.rectangle("fill", (self.x - (self.x_offset * self.scaleX)), (self.y - (self.y_offset * self.scaleY)), self.width, self.height)
 	love.graphics.setColor(r,g,b,a) -- Reset to old color
-
-	--love.graphics.setColor()
 end
 
 function cObject:draw()
 	love.graphics.draw(self.sprite, self.x, self.y, self.rotation, self.scaleX, self.scaleY, self.x_offset, self.y_offset)
-end
-
-function cObject:setLocation(x, y)
-	self.x = x
-	self.y = y
 end
 
 function cObject:move()
