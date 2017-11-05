2858f4bd83f679e084ee5db2be1bf4af4db7bdfd
Lane Gramling <lanegramling@gmail.com>
2017-11-04 23:03:45 -0500

    Including Server file in /Server and a pre-packaged plug-n-play .exe in /Rel.

--- netServer.lua
+++ netServer.lua
@@ -4,18 +4,17 @@ local socket = require "socket"
 
 
 -- Connection information
-local address, port = "*", "25560"
+local address, port = "*", "5050"
 local entity, cmd, parms
 local running = true -- Whether server is running. Here, we auto-start
 
 -- Initialize the server
 udp = socket.udp()
-udp:setsockname("*",25560)
+udp:setsockname(address, port)
 udp:settimeout(0)
 
 
-connectedPlayers = {}
-
+players = {}
 
 
 print "Beginning data receiver server loop..."
@@ -30,11 +29,15 @@ while running do
         entity, cmd, parms = data:match("^(%S*) (%S*) (.*)")
 
         -- Handle the various commands
-        if cmd == 'join' then playerJoinHandle(ent, fromIP, fromPort)
-        elseif cmd == 'leave' then playerLeaveHandle(ent, fromIP, fromPort)
+        if cmd == 'join' then
+          local lx,ly,lr,lg,lb = parms:match("(-*%d+.*%d*),(-*%d+.*%d*) (%d+),(%d+),(%d+)")
+          players[#players+1] = {id=entity, x=lx, y=ly, r=lr, g=lg, b=lb}
+        elseif cmd == 'leave' then
+          table.remove(players, getIndexOf(entity))
         elseif cmd == 'moveto' then
     			local x, y = parms:match("^(%-?[%d.e]*) (%-?[%d.e]*)$")
-
+          local i = indexOf(ent)
+    			players[i].x, players[i].y = x, y
         end
 
 
@@ -46,12 +49,17 @@ while running do
   socket.sleep(0.01)
 end
 
-
-function playerJoinHandle(ent, fromIP, fromPort)
-  table.insert(connectedPlayers, ent)
-  connectedPlayers[ent] = {fromIP, fromPort}
+-- Get the index of a player in the connected players list
+function indexOf(ent)
+	for i = 1, #players do
+		if players[i].id == ent then return i end
+	end
 end
 
-function playerLeaveHandle(ent, fromIP, fromPort)
-  table.remove(connectedPlayers, ent)
+function retrievePlayerList()
+  for i=1, #players do
+    data = string.format("1 %s %f,%f %d,%d,%d", players[i].id, players[i].x, players[i].y, players[i].r, players[i].g, players[i].b)
+    print(data)
+    -- udp:sendto(data, fromIP, fromPort)
+  end
 end
