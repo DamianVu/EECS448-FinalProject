fa6571c4cdf8b3abaafe7d10d7cd68514a26884e
Damian Vu <contact@damianvu.com>
2017-11-14 13:27:57 -0600

    HUD class

---
+++ Game/libraries/classes/HUD.lua
@@ -0,0 +1,15 @@
+
+HUD = class("HUD", {})
+
+function HUD:init()
+	-- "Default constructor" occurs when we initialize the HUD
+end
+
+function HUD:draw()
+
+	self:drawPlayerHP()
+end
+
+function HUD:drawPlayerHP()
+
+end
