645622d807d43ab974871d6257e515fe00e1b71e
Damian Vu <contact@damianvu.com>
2017-11-24 14:21:28 -0600

    Changed server ip back to normal

--- Game/GAMECONSTANTS.lua
+++ Game/GAMECONSTANTS.lua
@@ -1,6 +1,6 @@
 
 -- NETWORK CONSTANTS
-SERVER_ADDRESS, SERVER_PORT = "vuhoo.org", 5050
+SERVER_ADDRESS, SERVER_PORT = "13.58.15.46", 5050
 USERNAME = "Lane" -- String eventually
 updateRate = 0.1
 
