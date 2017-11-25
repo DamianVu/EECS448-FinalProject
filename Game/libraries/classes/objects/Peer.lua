84880555f5520c5c68ebd2ec46bbf53493a3aae3
Damian <damian@vuhoo.org>
2017-11-24 22:55:07 -0600

    Testing on multiple computers

--- Game/libraries/classes/objects/Peer.lua
+++ Game/libraries/classes/objects/Peer.lua
@@ -1,6 +1,28 @@
 
 Peer = class("Peer", {})
 
-function Peer:init()
+function Peer:init(id, color, x, y, size)
+	self.id = id
+	self.type = PEER
+	self.sprite = spriteImg
+	self.color = color
+	self.currentColor = self.color
+	self.x = x
+	self.y = y
+	self.width = size
+	self.height = size
 
+	local imgW, imgH = self.sprite:getDimensions()
+
+	self.x_offset = imgW / 2
+	self.y_offset = imgH / 2
+
+
+	self.scaleX = self.width / imgW
+	self.scaleY = self.height / imgH
+end
+
+function Peer:draw()
+	love.graphics.setColor(self.currentColor)
+	love.graphics.draw(self.sprite, self.x, self.y, 0, self.scaleX, self.scaleY, self.x_offset, self.y_offset)
 end
