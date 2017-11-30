b58af120f949f424b6dbd6b994901ac623558294
Kari Cieslak <k882c352@ku.edu>
2017-11-30 15:36:01 -0600

    added garden map

---
+++ Game/resources/tilesets/garden.lua
@@ -0,0 +1,12 @@
+
+garden = {
+	name = "garden",
+	Width = 3, -- In tiles
+	Height = 3, -- In tiles
+	size = 7,
+	image = love.graphics.newImage("images/tilesets/forest_garden.png")
+}
+
+
+
+return garden
