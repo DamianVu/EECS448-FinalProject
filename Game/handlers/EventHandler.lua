c712e88929cf22d2dafb03b2bfbafb7da03209a2
dustinbingham <31479630+dustinbingham@users.noreply.github.com>
2017-11-30 15:10:13 -0600

    Created 3 different type of enimes
    lava_bug: spawns somewhat often and very fast (not faster than you)
    angy_tourist: spawns very often but very slow.
    angry_ghost: spawns rarely but very fast (faster than you)

--- Game/handlers/EventHandler.lua
+++ Game/handlers/EventHandler.lua
@@ -16,11 +16,15 @@ function EventHandler:update(dt)
 		self.timer = self.timer - self.updateRate
 	end
 end
-
 function EventHandler:triggerEvent(EVENT)
-	if math.random(2) == 1 then
-		for i = 1, math.random(4) do
-			GH:addObject(Enemy(GH:getNewUID(), nil, nil, .5, 1, math.random(96,400), math.random(96,400), 32, 32, 5, 1))
+	if math.random(3) == 1 then
+		--for i = 1, math.random(4) do
+			GH:addObject(Enemy(GH:getNewUID(), love.graphics.newImage('images/sprites/lava_bug.png'), {255,255,255}, .99, 1, math.random(96,400), math.random(96,400), 32, 32, 5, 1))
 		end
-	end
+	if math.random(2) == 1 then
+		GH:addObject(Enemy(GH:getNewUID(), love.graphics.newImage('images/sprites/angry_tourist.png'), {255,255,255}, .2, 1, math.random(96,400), math.random(96,400), 32, 32, 5, 1))
+end
+if math.random(20) == 1 then
+	GH:addObject(Enemy(GH:getNewUID(), love.graphics.newImage('images/sprites/angry_ghost.png'), {255,255,255}, 1.1, 1, math.random(96,400), math.random(96,400), 32, 32, 5, 1))
+end
 end
